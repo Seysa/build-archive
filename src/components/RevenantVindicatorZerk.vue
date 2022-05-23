86aedfd31fe20ade3db27511166b7931a67c3763
Seysa <garion.g@hotmail.fr>
2022-05-23 23:07:26 +0200

    centered traits and stuff

--- src/components/RevenantVindicatorZerk.vue
+++ src/components/RevenantVindicatorZerk.vue
@@ -6,105 +6,107 @@
   </div>
   <div
     id="skills"
-    class="flex flex-col gap-2"
+    class="flex flex-col gap-2 mx-auto"
     data-armory-ids="9,3,69"
     data-armory-9-traits="1810,1782,1779"
     data-armory-3-traits="1732,1760,1749"
     data-armory-69-traits="2258,2255,2238"
     data-armory-embed="specializations"
   ></div>
-  <div id="weapons" class="mt-2">
-    <div id="first-weapon">
-      <div class="flex gap-2" id="eq-box-2">
-        <div>
+  <div id="stuff" class="flex gap-2 mt-2 justify-center">
+    <div id="weapons" class="flex items-center flex-col">
+      <div id="first-weapon">
+        <div class="flex gap-2" id="eq-box-2">
+          <div>
+            <div
+              data-armory-embed="items"
+              data-armory-ids="31652"
+              data-armory-blank-text="ID not found"
+            ></div>
+            <small>Greatsword</small>
+          </div>
           <div
             data-armory-embed="items"
-            data-armory-ids="31652"
-            data-armory-blank-text="ID not found"
+            data-armory-ids="-1"
+            data-armory-size="50"
+            data-armory-blank-text="Empty slot"
           ></div>
-          <small>Greatsword</small>
+          <div>
+            <div
+              data-armory-embed="items"
+              data-armory-ids="21150"
+              data-armory-size="46"
+              data-armory-blank-text="ID not found"
+            ></div>
+            <small>Sigil</small>
+          </div>
+          <div>
+            <div
+              data-armory-embed="items"
+              data-armory-ids="21152"
+              data-armory-size="46"
+              data-armory-blank-text="ID not found"
+            ></div>
+            <small>Sigil</small>
+          </div>
         </div>
-        <div
-          data-armory-embed="items"
-          data-armory-ids="-1"
-          data-armory-size="50"
-          data-armory-blank-text="Empty slot"
-        ></div>
-        <div>
+      </div>
+      <div id="second-weapon">
+        <div class="flex gap-2" id="eq-box-2">
+          <div>
+            <div
+              data-armory-embed="items"
+              data-armory-ids="31291"
+              data-armory-blank-text="ID not found"
+            ></div>
+            <small>Staff</small>
+          </div>
           <div
             data-armory-embed="items"
-            data-armory-ids="21150"
-            data-armory-size="46"
-            data-armory-blank-text="ID not found"
+            data-armory-ids="-1"
+            data-armory-size="50"
+            data-armory-blank-text="Empty slot"
           ></div>
-          <small>Sigil</small>
-        </div>
-        <div>
-          <div
-            data-armory-embed="items"
-            data-armory-ids="21152"
-            data-armory-size="46"
-            data-armory-blank-text="ID not found"
-          ></div>
-          <small>Sigil</small>
+          <div>
+            <div
+              data-armory-embed="items"
+              data-armory-ids="81245"
+              data-armory-size="46"
+              data-armory-blank-text="ID not found"
+            ></div>
+            <small>Sigil</small>
+          </div>
+          <div>
+            <div
+              data-armory-embed="items"
+              data-armory-ids="21152"
+              data-armory-size="46"
+              data-armory-blank-text="ID not found"
+            ></div>
+            <small>Sigil</small>
+          </div>
         </div>
       </div>
     </div>
-    <div id="second-weapon">
-      <div class="flex gap-2" id="eq-box-2">
-        <div>
-          <div
-            data-armory-embed="items"
-            data-armory-ids="31291"
-            data-armory-blank-text="ID not found"
-          ></div>
-          <small>Staff</small>
-        </div>
+    <div class="flex flex-col gap-5">
+      <div>
         <div
           data-armory-embed="items"
-          data-armory-ids="-1"
-          data-armory-size="50"
-          data-armory-blank-text="Empty slot"
+          data-armory-ids="21175"
+          data-armory-size="46"
+          data-armory-blank-text="ID not found"
         ></div>
-        <div>
-          <div
-            data-armory-embed="items"
-            data-armory-ids="81245"
-            data-armory-size="46"
-            data-armory-blank-text="ID not found"
-          ></div>
-          <small>Sigil</small>
-        </div>
-        <div>
-          <div
-            data-armory-embed="items"
-            data-armory-ids="21152"
-            data-armory-size="46"
-            data-armory-blank-text="ID not found"
-          ></div>
-          <small>Sigil</small>
-        </div>
+        <small>Rune</small>
       </div>
-    </div>
-  </div>
-  <div class="flex gap-5">
-    <div>
-      <div
-        data-armory-embed="items"
-        data-armory-ids="21175"
-        data-armory-size="46"
-        data-armory-blank-text="ID not found"
-      ></div>
-      <small>Rune</small>
-    </div>
-    <div>
-      <div
-        data-armory-embed="amulets"
-        data-armory-ids="8"
-        data-armory-size="46"
-        data-armory-blank-text="ID not found"
-      ></div>
-      <small>Amulet</small>
+      <div>
+        <div
+          data-armory-embed="amulets"
+          data-armory-ids="8"
+          data-armory-size="46"
+          data-armory-blank-text="ID not found"
+        ></div>
+        <small>Amulet</small>
+      </div>
     </div>
   </div>
 </template>
