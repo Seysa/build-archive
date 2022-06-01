3863c6cd00123c900a8b1c1bd18640dfea2e5b55
Seysa <garion.g@hotmail.fr>
2022-06-01 23:00:11 +0200

    added vue router

---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import Home from "../views/Home.vue";
+
+const importView = (viewName: string) => {
+  const capitalize = (str: string) => {
+    return str[0].toUpperCase() + str.slice(1);
+  };
+  return () => import(`../views/${capitalize(viewName)}.vue`);
+};
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/traitmaker",
+    name: "Trait Maker",
+    component: importView("traitMaker"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
