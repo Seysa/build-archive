fd7f20e9aed614ce157834ee2b14eddd196902f5
Seysa <garion.g@hotmail.fr>
2022-06-02 18:52:29 +0200

    fixed builds by forcing load with a tag

--- src/views/Builds.vue
+++ src/views/Builds.vue
@@ -4,24 +4,20 @@
     <div class="ml-2">
       <h2 class="text-3xl flex gap-2"><LogoVindicator />Vindicator</h2>
       <div class="ml-[0.93rem] pl-2 border-l-2 border-orange-500 my-2">
-        <router-link to="/builds/vindicator/full-zerk" class="text-2xl block"
-          >full zerk</router-link
+        <a href="/builds/vindicator/full-zerk" class="text-2xl block"
+          >full zerk</a
         >
-        <router-link to="/builds/vindicator/50-50-zerk" class="text-2xl block"
-          >50-50 zerk</router-link
+        <a href="/builds/vindicator/50-50-zerk" class="text-2xl block"
+          >50-50 zerk</a
         >
-        <router-link to="/builds/vindicator/tanky-zerk" class="text-2xl block"
-          >tanky zerk</router-link
+        <a href="/builds/vindicator/tanky-zerk" class="text-2xl block"
+          >tanky zerk</a
         >
       </div>
       <h2 class="text-3xl flex gap-2"><LogoHerald /> Herald</h2>
       <div class="ml-[0.93rem] pl-2 border-l-2 border-sky-500 my-2">
-        <router-link to="/builds/herald/power" class="text-2xl block"
-          >power</router-link
-        >
-        <router-link to="/builds/herald/power" class="text-2xl block"
-          >condi</router-link
-        >
+        <a href="/builds/herald/power" class="text-2xl block">power</a>
+        <a href="/builds/herald/power" class="text-2xl block">condi</a>
       </div>
     </div>
   </div>
