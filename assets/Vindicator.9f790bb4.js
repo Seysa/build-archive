e5d6432a31c784cd97ea1cbfd3850bf769e7854a
Seysa <57998113+Seysa@users.noreply.github.com>
2022-06-02 13:02:35 +0000

    Deploying to gh-pages from @ Seysa/build-archive@63d8fb9aa05504fdde49cb4a2cfcd6dc598d4558 🚀

---
+++ assets/Vindicator.9f790bb4.js
@@ -0,0 +1,1 @@
+import{d as t,o as n,c as o,t as a}from"./index.013b19b1.js";const s=t({props:{name:null},setup(e){return(c,l)=>(n(),o("div",null,"Welcome to "+a(e.name),1))}});export{s as default};
