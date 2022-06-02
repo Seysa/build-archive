a917d347c38daab41b53057f2f06c9e36a820cbf
Seysa <garion.g@hotmail.fr>
2022-06-02 14:48:02 +0200

    added json format to traitMaker

--- src/views/TraitMaker.vue
+++ src/views/TraitMaker.vue
@@ -8,7 +8,7 @@
         <span>Enter your code: </span
         ><input
           type="text"
-          class="p-1 rounded w-[37rem]"
+          class="p-1 rounded w-[37rem] text-black"
           v-model="buildCode"
           placeholder="[&XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=]"
         />
@@ -19,14 +19,21 @@
         Submit
       </button>
     </form>
-    <div
-      id="result"
-      class="whitespace-pre-line flex flex-col justify-center mt-2"
-    >
-      <code v-if="outputHTML" class="bg-black text-white px-4 py-1 rounded">{{
-        outputHTML
-      }}</code>
-      <h2 v-else-if="clicked">Chargement...</h2>
+    <div id="result" class="flex flex-col justify-center mt-2">
+      <h2 class="text-red-500 text-2xl text-center" v-if="invalidCode">
+        Code is invalid
+      </h2>
+      <div v-if="outputHTML" class="flex flex-col gap-4">
+        <h3 class="text-4xl">HTML</h3>
+        <pre class="bg-black text-white px-4 py-1 rounded">{{
+          outputHTML
+        }}</pre>
+        <h3 class="text-4xl">JSON</h3>
+        <pre id="JSON" class="bg-black text-white px-4 py-1 rounded block">{{
+          outputJSON
+        }}</pre>
+      </div>
+      <h2 v-else-if="clicked && !invalidCode">Chargement...</h2>
     </div>
   </div>
 </template>
@@ -39,22 +46,35 @@ const buildCode = ref(
 );
 
 const outputHTML = ref("");
+const outputJSON = ref("");
+const invalidCode = ref(false);
 const clicked = ref(false);
 
 async function showBuild() {
   clicked.value = true;
   const traitsAttributes: string[] = [];
-  const build = await correctBuildTemplate(buildCode.value);
-  build.specializations.forEach((trait) => {
-    traitsAttributes.push(
-      `data-armory-${trait.id}-traits="${trait.traits.join(",")}"`
-    );
-  });
-  const dataArmoryIds = build.specializations.map((el) => el.id).join(",");
-  outputHTML.value = `<div
+  try {
+    const build = await correctBuildTemplate(buildCode.value);
+
+    build.specializations.forEach((trait) => {
+      traitsAttributes.push(
+        `data-armory-${trait.id}-traits="${trait.traits.join(",")}"`
+      );
+    });
+    const dataArmoryIds = build.specializations.map((el) => el.id).join(",");
+    outputHTML.value = `<div
   data-armory-embed="specializations"
   data-armory-ids="${dataArmoryIds}"
-  ${traitsAttributes.join("\n")}
-  ></div>`;
+  ${traitsAttributes.join("\n  ")}
+></div>`;
+    outputJSON.value = JSON.stringify(build, null, 4);
+
+    console.log("html", outputHTML);
+    console.log("json", outputJSON);
+  } catch (e) {
+    outputHTML.value = "";
+    outputJSON.value = "";
+    invalidCode.value = true;
+  }
 }
 </script>
