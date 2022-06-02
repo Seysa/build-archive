577b91c1459f2c63cae01df82b833931067a02b2
Seysa <garion.g@hotmail.fr>
2022-06-02 23:24:43 +0200

    created navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <img id="background" :src="background" class="w-full h-full fixed -z-10" />
-  <nav class="flex justify-center gap-8 text-2xl text-white transition-all">
-    <a href="/">Home</a>
-    <router-link to="/traitmaker">Trait Maker</router-link>
-    <router-link to="/builds">Builds</router-link>
-  </nav>
+  <NavBar />
   <main>
     <h1 class="text-4xl text-white font-bold text-center p-2">
       Garion's builds
@@ -17,17 +13,5 @@
 
 <script lang="ts" setup>
 import background from "./assets/background.jpg";
+import NavBar from "./components/NavBar.vue";
 </script>
-
-<style>
-nav a {
-  @apply px-2;
-}
-
-nav a.router-link-exact-active {
-  @apply border-b-2 border-white;
-}
-nav a:hover {
-  @apply border-b-2 border-black;
-}
-</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="flex justify-center gap-8 text-2xl text-white transition-all">
+    <router-link to="/">Home</router-link>
+    <router-link to="/builds">Builds</router-link>
+    <router-link to="/traitmaker">Trait Maker</router-link>
+  </nav>
+</template>
+
+<style scoped>
+nav a {
+  @apply px-2;
+}
+
+nav a.router-link-exact-active {
+  @apply border-b-2 border-white;
+}
+nav a:hover {
+  @apply border-b-2 border-black;
+}
+</style>
