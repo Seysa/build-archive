2b1cd64ee4958537d77f350ef423b2cb03413d9f
Seysa <garion.g@hotmail.fr>
2022-06-02 18:56:13 +0200

    minor: css

--- src/views/Builds.vue
+++ src/views/Builds.vue
@@ -4,20 +4,14 @@
     <div class="ml-2">
       <h2 class="text-3xl flex gap-2"><LogoVindicator />Vindicator</h2>
       <div class="ml-[0.93rem] pl-2 border-l-2 border-orange-500 my-2">
-        <a href="/builds/vindicator/full-zerk" class="text-2xl block"
-          >full zerk</a
-        >
-        <a href="/builds/vindicator/50-50-zerk" class="text-2xl block"
-          >50-50 zerk</a
-        >
-        <a href="/builds/vindicator/tanky-zerk" class="text-2xl block"
-          >tanky zerk</a
-        >
+        <a href="/builds/vindicator/full-zerk" class="build">full zerk</a>
+        <a href="/builds/vindicator/50-50-zerk" class="build">50-50 zerk</a>
+        <a href="/builds/vindicator/tanky-zerk" class="build">tanky zerk</a>
       </div>
       <h2 class="text-3xl flex gap-2"><LogoHerald /> Herald</h2>
       <div class="ml-[0.93rem] pl-2 border-l-2 border-sky-500 my-2">
-        <a href="/builds/herald/power" class="text-2xl block">power</a>
-        <a href="/builds/herald/power" class="text-2xl block">condi</a>
+        <a href="/builds/herald/power" class="build">power</a>
+        <a href="/builds/herald/condi" class="build">condi</a>
       </div>
     </div>
   </div>
@@ -28,3 +22,9 @@ import LogoVindicator from "../components/logos/LogoVindicator.vue";
 import LogoHerald from "../components/logos/LogoHerald.vue";
 import LogoCore from "../components/logos/LogoCore.vue";
 </script>
+
+<style scoped>
+.build {
+  @apply text-2xl block p-2 hover:bg-gray-800 bg-opacity-10 rounded-lg;
+}
+</style>
