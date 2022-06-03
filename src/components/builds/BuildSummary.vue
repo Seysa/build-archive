12d1de0d592bd1c9157934239579eafece8e3c13
Seysa <garion.g@hotmail.fr>
2022-06-03 20:09:26 +0200

    used component for Builds

---
+++ src/components/builds/BuildSummary.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-link
+    class="text-2xl p-2 hover:bg-gray-800 rounded-lg"
+    :to="`/builds/${name}/${build.name}`"
+  >
+    <h3 class="ml-2">{{ build.name }}</h3>
+    <span class="text-lg text-gray-400"
+      >{{ build.value.amulet }} - {{ build.value.rune }}</span
+    ></router-link
+  >
+</template>
+
+<script lang="ts" setup>
+import { Build } from "../../utils/builds";
+defineProps<{
+  build: { value: Build; name: string };
+  name: string;
+}>();
+</script>
--- src/views/Builds.vue
+++ src/views/Builds.vue
@@ -4,23 +4,21 @@
     <div class="ml-2">
       <h2 class="text-3xl flex gap-2"><LogoVindicator />Vindicator</h2>
       <div class="build-container border-orange-500">
-        <router-link
-          class="build hover:text-orange-300"
-          v-for="(value, field) in vindicator"
-          :to="`/builds/vindicator/${field}`"
-        >
-          {{ field }} - {{ value.amulet }} - {{ value.rune }}</router-link
-        >
+        <BuildSummary
+          v-for="(value, name) in vindicator"
+          :build="{ value, name }"
+          name="vindicator"
+          class="hover:text-orange-300"
+        />
       </div>
       <h2 class="text-3xl flex gap-2"><LogoHerald /> Herald</h2>
       <div class="build-container border-sky-500">
-        <router-link
-          class="build hover:text-sky-300"
-          v-for="(value, field) in herald"
-          :to="`/builds/herald/${field}`"
-        >
-          {{ field }} - {{ value.amulet }} - {{ value.rune }}</router-link
-        >
+        <BuildSummary
+          v-for="(value, name) in herald"
+          :build="{ value, name }"
+          name="herald"
+          class="hover:text-sky-300"
+        />
       </div>
     </div>
   </div>
@@ -31,13 +29,11 @@ import LogoVindicator from "../components/logos/LogoVindicator.vue";
 import LogoHerald from "../components/logos/LogoHerald.vue";
 import LogoCore from "../components/logos/LogoCore.vue";
 import { vindicator, herald } from "../utils/builds";
+import BuildSummary from "../components/builds/BuildSummary.vue";
 </script>
 
 <style scoped>
 .build-container {
   @apply ml-[0.93rem] pl-2 border-l-2  my-2 flex flex-col gap-2;
-}
-.build {
-  @apply text-2xl block p-2 hover:bg-gray-800 rounded-lg;
 }
 </style>
